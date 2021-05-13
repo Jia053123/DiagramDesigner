ae3720665491120fe14f36cbbcf30ea5be5e0f60
ErnestX <xiangspecial2012@gmail.com>
2021-05-13 17:18:41 -0400

    add gitignore; stubs

---
+++ DiagramDesigner/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiagramDesigner
+{
+    abstract class Entity
+    {
+        public Geometry geometry { get; protected set; }
+
+        public void Draw()
+        {
+            // TODO: stub
+            this.geometry.Draw();
+        }
+    }
+}
---
+++ DiagramDesigner/Geometry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiagramDesigner
+{
+    abstract class Geometry
+    {
+        public Double translationX = 0;
+        public Double translationY = 0;
+        public Double rotation = 0;
+        abstract public void Draw();
+    }
+}
