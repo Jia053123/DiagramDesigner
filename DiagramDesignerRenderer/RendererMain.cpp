befcc62b022401eff9cfa22a87f3304a5182d3ae
ErnestX <xiangspecial2012@gmail.com>
2021-07-09 18:09:58 -0400

    add demo project for Direct2D

---
+++ DiagramDesignerRenderer/RendererMain.cpp
@@ -0,0 +1,121 @@
+#include "RendererHeader.h"
+
+DiagramDesignerRenderer::DiagramDesignerRenderer() :
+    m_hwnd(NULL),
+    m_pDirect2dFactory(NULL),
+    m_pRenderTarget(NULL),
+    m_pLightSlateGrayBrush(NULL),
+    m_pCornflowerBlueBrush(NULL)
+{
+}
+
+DiagramDesignerRenderer::~DiagramDesignerRenderer() {
+    SafeRelease(&m_pDirect2dFactory);
+    SafeRelease(&m_pRenderTarget);
+    SafeRelease(&m_pLightSlateGrayBrush);
+    SafeRelease(&m_pCornflowerBlueBrush);
+}
+
+void DiagramDesignerRenderer::RunMessageLoop()
+{
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+HRESULT DiagramDesignerRenderer::Initialize()
+{
+    HRESULT hr;
+
+    // Initialize device-indpendent resources, such
+    // as the Direct2D factory.
+    hr = CreateDeviceIndependentResources();
+
+    if (SUCCEEDED(hr))
+    {
+        // Register the window class.
+        WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+        wcex.style = CS_HREDRAW | CS_VREDRAW;
+        wcex.lpfnWndProc = DiagramDesignerRenderer::WndProc;
+        wcex.cbClsExtra = 0;
+        wcex.cbWndExtra = sizeof(LONG_PTR);
+        wcex.hInstance = HINST_THISCOMPONENT;
+        wcex.hbrBackground = NULL;
+        wcex.lpszMenuName = NULL;
+        wcex.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+        wcex.lpszClassName = L"D2DDemoApp";
+
+        RegisterClassEx(&wcex);
+
+
+        // Because the CreateWindow function takes its size in pixels,
+        // obtain the system DPI and use it to scale the window size.
+        FLOAT dpiX, dpiY;
+
+        // The factory returns the current system DPI. This is also the value it will use
+        // to create its own windows.
+        //m_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+        dpiX = GetDpiForWindow(GetDesktopWindow());
+        dpiY = dpiX;
+
+
+
+        // Create the window.
+        m_hwnd = CreateWindow(
+            L"D2DDemoApp",
+            L"Direct2D Demo App",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            static_cast<UINT>(ceil(640.f * dpiX / 96.f)),
+            static_cast<UINT>(ceil(480.f * dpiY / 96.f)),
+            NULL,
+            NULL,
+            HINST_THISCOMPONENT,
+            this
+        );
+        hr = m_hwnd ? S_OK : E_FAIL;
+        if (SUCCEEDED(hr))
+        {
+            ShowWindow(m_hwnd, SW_SHOWNORMAL);
+            UpdateWindow(m_hwnd);
+        }
+    }
+
+    return hr;
+}
+
+
+int WINAPI WinMain(
+    HINSTANCE /* hInstance */,
+    HINSTANCE /* hPrevInstance */,
+    LPSTR /* lpCmdLine */,
+    int /* nCmdShow */
+)
+{
+    // Use HeapSetInformation to specify that the process should
+    // terminate if the heap manager detects an error in any heap used
+    // by the process.
+    // The return value is ignored, because we want to continue running in the
+    // unlikely event that HeapSetInformation fails.
+    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+    if (SUCCEEDED(CoInitialize(NULL)))
+    {
+        {
+            DiagramDesignerRenderer app;
+
+            if (SUCCEEDED(app.Initialize()))
+            {
+                app.RunMessageLoop();
+            }
+        }
+        CoUninitialize();
+    }
+
+    return 0;
+}
