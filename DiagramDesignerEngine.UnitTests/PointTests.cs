5882e8f8d7b9539b6a27782a73b156ed8f37f22c
ErnestX <xiangspecial2012@gmail.com>
2021-08-03 17:28:28 -0400

    test point

--- DiagramDesignerEngine.UnitTests/PointTests.cs
+++ DiagramDesignerEngine.UnitTests/PointTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using DiagramDesignerEngine;
 
 namespace DiagramDesignerEngine.UnitTests
 {
@@ -10,9 +11,34 @@ namespace DiagramDesignerEngine.UnitTests
 		}
 
 		[Test]
-		public void Test1()
+		public void TestEqualities()
 		{
-			Assert.Pass();
+			var p1 = new Point(1, 2);
+			var p2 = new Point(1, 2);
+			
+			var p3 = new Point(1, 2.02);
+			var p4 = new Point(0.9, 2);
+
+			var p5 = new Point(1.0004, 1.9991);
+
+			Point p6 = null;
+
+			Assert.AreEqual(p1, p2);
+			Assert.AreNotEqual(p3, p1);
+			Assert.AreNotEqual(p1, p4);
+			Assert.AreEqual(p1, p5);
+			Assert.AreNotEqual(p1, p6);
+
+			Assert.IsTrue(p1 == p2);
+			Assert.IsTrue(p1 != p3);
+			Assert.IsTrue(p4 != p1);
+			Assert.IsTrue(p1 == p5);
+			Assert.IsTrue(p1 != p6);
+			Assert.IsTrue(p6 != p1);
+
+			Assert.IsTrue(p1.Equals(p2));
+			Assert.IsFalse(p1.Equals(p3));
+			Assert.IsFalse(p1.Equals(p6));
 		}
 	}
 }
