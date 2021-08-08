077ddce08a00c65ffadb00f27609e6ebdf9afbbb
ErnestX <xiangspecial2012@gmail.com>
2021-08-08 16:52:41 -0400

    tweak unit test

--- DiagramDesignerEngine.UnitTests/SegmentsExploderTests.cs
+++ DiagramDesignerEngine.UnitTests/SegmentsExploderTests.cs
@@ -31,28 +31,28 @@ namespace DiagramDesignerEngine.UnitTests
 			Assert.IsTrue(result1.Contains(new LineSegment(new Point(1, 0), new Point(3, 0))));
 		}
 
-			[Test]
+		[Test]
 		public void TestExplodeSegments_2()
 		{
 			// 
-			//      |
+			//            |
 			//   -------x----x-------
-			//      |     
-			// -3   -2 -1    1       3 
+			//            |     
+			// -3      -1 0  1       3 
 			//
 			var ls1 = new LineSegment(new Point(-1, 0), new Point(1, 0));
 			var ls2 = new LineSegment(new Point(-3, 0), new Point(3, 0));
-			var ls3 = new LineSegment(new Point(-2, -1), new Point(-2, 1));
+			var ls3 = new LineSegment(new Point(0, -1), new Point(0, 1));
 			var segments1 = new List<LineSegment> { ls1, ls3, ls2 };
 
 			var exploder1 = new LineSegmentsExploder(segments1);
 			var result1 = exploder1.ExplodeSegments();
 			Assert.AreEqual(result1.Count, 6);
-			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-3, 0), new Point(-2, 0))));
-			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-2, -1), new Point(-2, 0))));
-			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-2, 1), new Point(-2, 0))));
-			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-2, 0), new Point(-1, 0))));
-			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-1, 0), new Point(1, 0))));
+			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-3, 0), new Point(-1, 0))));
+			Assert.IsTrue(result1.Contains(new LineSegment(new Point(-1, 0), new Point(0, 0))));
+			Assert.IsTrue(result1.Contains(new LineSegment(new Point(0, -1), new Point(0, 0))));
+			Assert.IsTrue(result1.Contains(new LineSegment(new Point(0, 1), new Point(0, 0))));
+			Assert.IsTrue(result1.Contains(new LineSegment(new Point(0, 0), new Point(1, 0))));
 			Assert.IsTrue(result1.Contains(new LineSegment(new Point(1, 0), new Point(3, 0))));
 		}
 
