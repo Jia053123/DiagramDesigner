047a5955e725d7ddbaaffaa59711783ba5058417
ErnestX <xiangspecial2012@gmail.com>
2021-09-06 00:44:23 -0400

    implement shape definition

--- ShapeGrammarEngine/Shape.cs
+++ ShapeGrammarEngine/Shape.cs
@@ -4,8 +4,41 @@ using System.Text;
 
 namespace ShapeGrammarEngine
 {
-	public class Shape
+	public readonly struct Shape: IEquatable<Shape>
 	{
-		
+		public readonly HashSet<(int, int)> Definition;
+
+		public Shape(HashSet<(int, int)> definition)
+		{
+			this.Definition = definition;
+		}
+
+		public static bool operator ==(Shape lhs, Shape rhs)
+		{
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(Shape lhs, Shape rhs) => !(lhs == rhs);
+
+		public override bool Equals(object obj) => obj is Shape other && this.Equals(other);
+		bool IEquatable<Shape>.Equals(Shape other) => this.Equals(other);
+
+		private bool Equals(Shape s)
+		{
+			if (this.Definition.Count != s.Definition.Count)
+			{
+				return false;
+			}
+
+			foreach ((int, int) connection in this.Definition)
+			{
+				if (! (s.Definition.Contains(connection) || s.Definition.Contains((connection.Item2, connection.Item1))))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
 	}
 }
