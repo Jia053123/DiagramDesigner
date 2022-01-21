78ec441112519dee697d456c893a58646569d9b8
ErnestX <xiangspecial2012@gmail.com>
2022-01-21 16:04:52 -0500

    create DraftingConstrainsApplier class

---
+++ DiagramDesigner/DraftingConstrainsApplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinPoint = System.Windows.Point;
+
+namespace DiagramDesigner
+{
+	/// <summary>
+	/// Manage and apply the drafting constrains such as snapping and aligning. 
+	/// </summary>
+	class DraftingConstrainsApplier
+	{
+		public bool IsDrawingOrthogonally;
+		private List<List<WinPoint>> CurrentGeometries;
+
+		public DraftingConstrainsApplier(List<List<WinPoint>> currentGeometries, bool drawOrthogonally)
+		{
+			this.CurrentGeometries = currentGeometries;
+			this.IsDrawingOrthogonally = drawOrthogonally;
+		}
+	}
+}
