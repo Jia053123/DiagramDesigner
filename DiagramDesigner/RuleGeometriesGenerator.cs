b68c8bd97141f358de0f78b5d8c7eb0c99f2755e
ErnestX <xiangspecial2012@gmail.com>
2022-01-28 15:22:54 -0500

    create RuleGeometriesGenerator class and stubs

---
+++ DiagramDesigner/RuleGeometriesGenerator.cs
@@ -0,0 +1,26 @@
+﻿using ShapeGrammarEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WinPoint = System.Windows.Point;
+
+namespace DiagramDesigner
+{
+	/// <summary>
+	/// Generates left-hand and right-hand geometries based on highlighted walls
+	/// </summary>
+	class RuleGeometriesGenerator
+	{
+		public double displayUnitOverRealUnit = 1;
+
+		internal RuleGeometriesGenerator(double displayUnitOverRealUnit)
+		{
+			this.displayUnitOverRealUnit = displayUnitOverRealUnit;
+		}
+
+		internal Tuple<PolylinesGeometry, PolylinesGeometry> GenerateGeometriesFromContextAndAdditions(List<List<WinPoint>> AllWalls, List<Tuple<int, int, int>> ContextWalls, List<Tuple<int, int, int>> AdditionWalls)
+		{
+			
+		}
+	}
+}
