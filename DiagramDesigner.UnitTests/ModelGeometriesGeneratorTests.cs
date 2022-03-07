d5a0f0e359f9b168f53919f1a23c6174ab8a11d3
ErnestX <xiangspecial2012@gmail.com>
2022-03-07 14:20:31 -0500

    add ModelGeometriesGeneratorTests and stubs

---
+++ DiagramDesigner.UnitTests/ModelGeometriesGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace DiagramDesigner.UnitTests
+{
+	class ModelGeometriesGeneratorTests
+	{
+		[Test]
+		public void TestGenerateGeometriesFromContextAndAdditions_ContextIndexOutOfRange_ThrowArgumentException()
+		{
+			
+		}
+
+		[Test]
+		public void TestGenerateGeometriesFromContextAndAdditions_AdditionIndexOutOfRange_ThrowArgumentException()
+		{
+
+		}
+
+		[Test]
+		public void TestGenerateLeftHandGeometryFromContext_IndexOutOfRange_ThrowArgumentException()
+		{
+
+		}
+
+		[Test]
+		public void TestGenerateLeftHandGeometryFromContext_ValidArguments_CorrectOutput()
+		{
+			
+		}
+	}
+}
