5be850068cbb4b754ce0862e553f306d847f5601
ErnestX <xiangspecial2012@gmail.com>
2023-05-18 02:36:59 -0400

    add back unpadded version

--- ShapeGrammarEngine/Utilities.cs
+++ ShapeGrammarEngine/Utilities.cs
@@ -91,6 +91,57 @@ namespace ShapeGrammarEngine
 		/// <param name="strokeWidth"> Width of the stroke used the render the geometry; Recommend even number </param>
 		/// <returns> A SVG document with the specified dimensions </returns>
 		static internal SvgDocument PolylinesGeometryToSvg(PolylinesGeometry polyGeo, int width, int height, int strokeWidth)
+		{
+			SvgDocument shapeDoc = new SvgDocument();
+			shapeDoc.Width = width;
+			shapeDoc.Height = height;
+			shapeDoc.ViewBox = new SvgViewBox(0, 0, width, height);
+
+			var sp = new SvgPath();
+			var spsl = new SvgPathSegmentList();
+
+			var boundingBox = polyGeo.GetBoundingBox();
+			double xDelta = -1 * boundingBox.xMin;
+			double yDelta = -1 * boundingBox.yMax;
+			double xFactor = width / (boundingBox.xMax - boundingBox.xMin);
+			double yFactor = height / (boundingBox.yMax - boundingBox.yMin);
+			foreach (List<MyPoint> polyline in polyGeo.PolylinesCopy)
+			{
+				for (int i = 0; i < polyline.Count; i++)
+				{
+					MyPoint p = polyline[i];
+					int mappedX = Convert.ToInt32((p.coordinateX + xDelta) * xFactor);
+					int mappedY = -1 * Convert.ToInt32((p.coordinateY + yDelta) * yFactor);
+					if (i == 0)
+					{
+						spsl.Add(new SvgMoveToSegment(false, new PointF(mappedX, mappedY)));
+					}
+					else
+					{
+						spsl.Add(new SvgLineSegment(false, new PointF(mappedX, mappedY)));
+					}
+				}
+			}
+
+			sp.PathData = spsl;
+			sp.StrokeWidth = strokeWidth;
+			sp.Stroke = new SvgColourServer(Color.Black);
+			sp.Fill = null;
+
+			shapeDoc.Children.Add(sp);
+
+			return shapeDoc;
+		}
+
+		/// <summary>
+		/// Render a PolylinesGeometry object as svg. The polylines will be squeezed within the specified dimension with padding to accommodate the stroke width
+		/// </summary>
+		/// <param name="polyGeo"> The PolylineGeometry to render </param>
+		/// <param name="width"> Width of the resulting svg document </param>
+		/// <param name="height"> Height of the resulting svg document </param>
+		/// <param name="strokeWidth"> Width of the stroke used the render the geometry; Recommend even number </param>
+		/// <returns> A SVG document with the specified dimensions </returns>
+		static internal SvgDocument PolylinesGeometryToSvgPadded(PolylinesGeometry polyGeo, int width, int height, int strokeWidth)
         {
 			SvgDocument shapeDoc = new SvgDocument();
 			shapeDoc.Width = width;
--- ShapeGrammarEngineUnitTests/UtilitiesTests.cs
+++ ShapeGrammarEngineUnitTests/UtilitiesTests.cs
@@ -71,7 +71,7 @@ namespace ShapeGrammarEngineUnitTests
 
 		[Test]
 		public void TestPolylinesGeometryToSvg()
-        {
+		{
 			//  1        /|
 			//  0       / /
 			// -1       v    
@@ -83,13 +83,39 @@ namespace ShapeGrammarEngineUnitTests
 			var geo = new PolylinesGeometry(testPolyline);
 
 			SvgDocument polyGeoDoc = Utilities.PolylinesGeometryToSvg(geo, 128, 128, 2);
+			Assert.AreEqual("M0 128 L128 0 L128 64 L0 128", ((SvgPath)polyGeoDoc.Children[0]).PathData.ToString());
+
+			Bitmap bm = polyGeoDoc.Draw();
+			string testResultsDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GraphicsTestResults\\");
+			_ = System.IO.Directory.CreateDirectory(testResultsDir);
+
+			string fileName = nameof(this.TestPolylinesGeometryToSvg) + ".bmp";
+			string testResultPath = System.IO.Path.Combine(testResultsDir, fileName);
+			bm.Save(testResultPath);
+			Console.WriteLine("Test result saved to: " + testResultPath);
+		}
+
+		[Test]
+		public void TestPolylinesGeometryToSvgPadded()
+        {
+			//  1        /|
+			//  0       / /
+			// -1       v    
+			//
+			//         0   1       
+
+			var testPolyline = new List<List<MyPoint>> {
+				new List<MyPoint> { new MyPoint(0, -1), new MyPoint(1, 1), new MyPoint(1, 0), new MyPoint(0, -1) } };
+			var geo = new PolylinesGeometry(testPolyline);
+
+			SvgDocument polyGeoDoc = Utilities.PolylinesGeometryToSvgPadded(geo, 128, 128, 2);
 			Assert.AreEqual("M1 127 L127 1 L127 64 L1 127", ((SvgPath)polyGeoDoc.Children[0]).PathData.ToString());
 
             Bitmap bm = polyGeoDoc.Draw();
             string testResultsDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GraphicsTestResults\\");
 			_ = System.IO.Directory.CreateDirectory(testResultsDir);
 
-			string fileName = nameof(this.TestPolylinesGeometryToSvg) + ".bmp";
+			string fileName = nameof(this.TestPolylinesGeometryToSvgPadded) + ".bmp";
 			string testResultPath = System.IO.Path.Combine(testResultsDir, fileName);
 			bm.Save(testResultPath);
 			Console.WriteLine("Test result saved to: " + testResultPath);
